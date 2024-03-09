e0902096ecfdcb05d498785283266665cf0505c8
AdrianBurgosA <aaburgos1@espe.edu.ec>
2024-03-09 11:09:10 -0500

    first push

---
+++ serviciowpp/src/main/java/com/example/serviciowpp/ServiciowppApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciowpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciowppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciowppApplication.class, args);
+	}
+
+}
---
+++ serviciowpp/src/test/java/com/example/serviciowpp/ServiciowppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciowpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciowppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
